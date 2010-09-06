2f8b2d3776d2242939e3c3e1e38fbed4f6f97d49
Lal'C Mellk Mal <lalc.mellk.mal@gmail.com>
2010-09-06 12:11:38 -0700

    Better builder tracking; track tripcode.node

--- builder.c
+++ builder.c
@@ -42,6 +42,7 @@ static void setup_watches(void) {
 	add_watch("client.js", &build_client);
 	add_watch("config.js", &build_client);
 	add_watch("server.js", &restart_server);
+	add_watch("tripcode.node", &restart_server);
 	add_watch("common.js", &client_and_server);
 }
 
@@ -55,37 +56,38 @@ static void kill_existing(const char *pid_file) {
 	}
 }
 
-#define MAX_WATCHES 4
-static struct { int wd; void (*func)(void); } watches[MAX_WATCHES];
+#define MAX_WATCHES 5
+static struct { const char *name; void (*func)(void); } watches[MAX_WATCHES];
 static int num_watches, inotify_fd;
 
 static void monitor_files(void) {
-	struct inotify_event event;
-	int i;
-	if (read(inotify_fd, &event, sizeof event) != sizeof event) {
+	struct inotify_event *event;
+	char buf[128];
+	int i, len, total;
+	if ((total = read(inotify_fd, buf, sizeof buf)) <= 0) {
 		fprintf(stderr, "Monitor failure\n");
 		exit(-1);
 	}
-	for (i = 0; i < MAX_WATCHES; i++) {
-		if (event.wd == watches[i].wd) {
-			(*watches[i].func)();
-			break;
+	event = (struct inotify_event *) buf;
+	while (total > 0) {
+		for (i = 0; i < MAX_WATCHES; i++) {
+			if (!strcmp(event->name, watches[i].name)) {
+				(*watches[i].func)();
+				break;
+			}
 		}
+		len = sizeof *event + event->len;
+		event = (struct inotify_event *) ((char *)event + len);
+		total -= len;
 	}
 }
 
 static void add_watch(const char *filename, void (*f)(void)) {
-	int w;
 	if (num_watches >= MAX_WATCHES) {
 		fprintf(stderr, "No slots available for more watches.\n");
 		return;
 	}
-	w = inotify_add_watch(inotify_fd, filename, IN_MODIFY);
-	if (w < 0) {
-		perror(filename);
-		return;
-	}
-	watches[num_watches].wd = w;
+	watches[num_watches].name = filename;
 	watches[num_watches].func = f;
 	num_watches++;
 }
@@ -117,6 +119,10 @@ int main(void) {
 	inotify_fd = inotify_init();
 	if (!inotify_fd) {
 		perror("inotify_init");
+		return -1;
+	}
+	if (inotify_add_watch(inotify_fd, ".", IN_CREATE | IN_MODIFY) < 0) {
+		perror(".");
 		return -1;
 	}
 	setup_watches();
