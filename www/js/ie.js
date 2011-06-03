152ba095280dece271f14ad6b383b2e4cdf02e28
Lal'C Mellk Mal <lalc.mellk.mal@gmail.com>
2011-06-03 00:33:26 -0700

    Add nav element to IE fix

--- www/js/ie.js
+++ www/js/ie.js
@@ -1,5 +1,5 @@
 var h5s = ['abbr', 'aside', 'article', 'code', 'figcaption', 'figure',
-		'section', 'time'];
+		'nav', 'section', 'time'];
 for (var i = 0; i < h5s.length; i++)
 	document.createElement(h5s[i]);
 window.onload = function () { $('h1').after('<h2>Google Chrome recommended.</h2>'); };
