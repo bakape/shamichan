35fd1e06c108440999579e32403ae2054273c6af
Lal'C Mellk Mal <lalc@doushio.com>
2012-01-02 15:32:03 -0800

    Per-post dice roll limit

--- server/games.js
+++ server/games.js
@@ -1,9 +1,11 @@
 var common = require('./common');
 
+var rollLimit = 5;
+
 exports.roll_dice = function (frag, post, extra) {
 	var ms = frag.split(common.dice_re);
 	var dice = [];
-	for (var i = 1; i < ms.length; i += 2) {
+	for (var i = 1; i < ms.length && dice.length < rollLimit; i += 2) {
 		var info = common.parse_dice(ms[i]);
 		if (!info)
 			continue;
@@ -16,14 +18,19 @@ exports.roll_dice = function (frag, post, extra) {
 		dice.push(rolls);
 	}
 	if (dice.length) {
-		extra.new_dice = dice;
-		dice = post.dice ? post.dice.concat(dice) : dice;
-		post.dice = dice;
 		// Would prefer an appending scheme for adding new rolls but
 		// there's no hash value append redis command...
 		// I don't want to spill into a separate redis list.
 		// Overwriting the whole log every time is quadratic though.
-		// TODO: Limit the total number of dice rolls per post
+		// Enforcing a roll limit to deter that and for sanity
+		var exist = post.dice ? post.dice.length : 0;
+		if (dice.length + exist > rollLimit)
+			dice = dice.slice(0, Math.max(0, rollLimit - exist));
+		if (dice.length) {
+			extra.new_dice = dice;
+			dice = post.dice ? post.dice.concat(dice) : dice;
+			post.dice = dice;
+		}
 	}
 };
 
