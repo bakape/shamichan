153a4aac4df8a95f45d86890836762f0d2d1933b
Lal'C Mellk Mal <lalc@doushio.com>
2012-07-05 17:02:08 +0800

    Style curfew notice

--- www/js/curfew-v1.js
+++ www/js/curfew-v1.js
@@ -2,16 +2,16 @@
 
 var BOARD = document.location.pathname.match(/\/(\w+)\/$/)[1];
 
-var $title = $('<h2/>', {text: '/' + BOARD + '/ is currently closed.'});
+var $title = $('<h2/>', {text: '/' + BOARD + '/ is out of season.'});
 var $when = $('<span/>');
-var $remain = $('<p>', {text: 'It will reopen in '}).append($when).append('.');
+var $remain = $('<p>', {text: 'Open in '}).append($when).append('.');
 var countdownInterval, ticks = 0;
 
 function countdown() {
 	var now = new Date();
 	var diff = Math.floor((END - now.getTime()) / 1000);
 	if (diff < 0.5) {
-		$remain.text('It will open SOON.');
+		$remain.text('Open SOON.');
 		clearInterval(countdownInterval);
 		if (ticks > 3)
 			history.go(0);
