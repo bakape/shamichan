151df8eede1c4854ebc2850b3593aed03736003f
Lal'C Mellk Mal <lalc@doushio.com>
2013-06-10 01:54:54 -0700

    Preliminary JSON dumping

--- upkeep/dump.js
+++ upkeep/dump.js
@@ -1,10 +1,48 @@
-var caps = require('../server/caps'),
+var _ = require('../lib/underscore'),
+    async = require('async'),
+    caps = require('../server/caps'),
+    fs = require('fs'),
     render = require('../server/render'),
     db = require('../db');
 
 var DUMP_IDENT = {ip: '127.0.0.1', auth: 'dump'};
 
-function dump_thread(op, board, ident, out, cb) {
+function Dumper(reader, out) {
+	this.out = out;
+	this.reader = reader;
+	_.bindAll(this);
+	reader.on('thread', this.on_thread);
+	reader.on('post', this.on_post);
+	reader.on('endthread', this.on_endthread);
+}
+var D = Dumper.prototype;
+
+D.on_thread = function (op_post) {
+	if (this.needComma) {
+		this.out.write(',\n');
+		this.needComma = false;
+	}
+	this.out.write('[\n\t' + JSON.stringify(op_post));
+};
+
+D.on_post = function (post) {
+	this.out.write(',\n\t' + JSON.stringify(post));
+};
+
+D.on_endthread = function () {
+	this.out.write('\n]');
+	this.needComma = true;
+};
+
+D.destroy = function () {
+	this.reader.removeListener('thread', this.on_thread);
+	this.reader.removeListener('post', this.on_post);
+	this.reader.removeListener('endthread', this.on_endthread);
+	this.reader = null;
+	this.out = null;
+};
+
+function dump_thread(op, board, ident, outputs, cb) {
 	if (!caps.can_access_board(ident, board))
 		return cb(404);
 	/*
@@ -25,6 +63,9 @@ function dump_thread(op, board, ident, out, cb) {
 		yaku.disconnect();
 	});
 	reader.once('begin', function (preThread) {
+		var dumper = new Dumper(reader, outputs.json);
+
+		var out = outputs.html;
 		render.write_thread_head(out, board, op, preThread.subject);
 
 		var fakeReq = {ident: ident, headers: {}};
@@ -32,6 +73,7 @@ function dump_thread(op, board, ident, out, cb) {
 		render.write_thread_html(reader, fakeReq, out, opts);
 
 		reader.once('end', function () {
+			outputs.json.write('\n');
 			render.write_page_end(out, ident, true);
 			yaku.disconnect();
 			cb(null);
@@ -46,29 +88,57 @@ function dump_thread(op, board, ident, out, cb) {
 	yaku.once('error', on_err);
 }
 
+function close_stream(stream, cb) {
+	if (!stream.writable)
+		return cb(null);
+	if (stream.write(''))
+		close();
+	else
+		stream.once('drain', close);
+
+	function close() {
+		try { stream.end(); } catch (e) {}
+		cb(null);
+	}
+}
+
+function load_state(cb) {
+	require('../server/state').reload_hot_resources(cb);
+}
+
 if (require.main === module) (function () {
 	var op = parseInt(process.argv[2], 10), board = process.argv[3];
 	if (!op || !board) {
 		console.error('Usage: node upkeep/dump.js <thread> <board>');
 		process.exit(-1);
 	}
+
 	console.log('Loading state...');
-	require('../server/state').reload_hot_resources(function (err) {
+	load_state(function (err) {
 		if (err)
 			throw err;
 		console.log('Dumping thread...');
-		dump_thread(op, board, DUMP_IDENT, process.stdout,
-				function (err) {
+
+		var outputs = {
+			json: process.stdout,
+			html: fs.createWriteStream('dump.html'),
+		};
+		var streams = [outputs.json, outputs.html];
+
+		dump_thread(op, board, DUMP_IDENT, outputs, function (err) {
 			if (err)
 				throw err;
 
-			// crappy flush()
-			if (process.stdout.write(''))
-				process.exit(0);
-			else
-				process.stdout.on('drain', function () {
+			async.each(streams, close_stream, function (err) {
+				// crappy flush for stdout (can't close it)
+				if (process.stdout.write(''))
 					process.exit(0);
-				});
+				else
+					process.stdout.on('drain', function () {
+						process.exit(0);
+					});
+			});
+
 		});
 	});
 })();
