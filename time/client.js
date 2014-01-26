b29eb395840f11f8a981040b2e75a9116f9def34
Lal'C Mellk Mal <lalc@doushio.com>
2014-01-25 18:10:50 -0800

    Optimize adjust_all_times() and extract helper

--- time/client.js
+++ time/client.js
@@ -2,11 +2,15 @@
 
 function adjust_all_times() {
 	$('time').each(function () {
-		var t = $(this);
-		var d = t.attr('datetime').replace(/-/g, '/'
-			).replace('T', ' ').replace('Z', ' GMT');
-		t.html(readable_time(new Date(d).getTime()));
+		var date = date_from_time_el(this);
+		this.innerHTML = readable_time(date.getTime());
 	});
+}
+
+function date_from_time_el(el) {
+	var d = el.getAttribute('datetime').replace(/-/g, '/'
+		).replace('T', ' ').replace('Z', ' GMT');
+	return new Date(d);
 }
 
 adjust_all_times();
