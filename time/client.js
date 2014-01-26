b7ed0a9e59e4b42b831834f36af141cfb2b4fdc9
Lal'C Mellk Mal <lalc@doushio.com>
2014-01-25 18:10:19 -0800

    Wrap time adjustment

--- time/client.js
+++ time/client.js
@@ -1,8 +1,14 @@
 (function () {
+
+function adjust_all_times() {
 	$('time').each(function () {
 		var t = $(this);
 		var d = t.attr('datetime').replace(/-/g, '/'
 			).replace('T', ' ').replace('Z', ' GMT');
 		t.html(readable_time(new Date(d).getTime()));
 	});
+}
+
+adjust_all_times();
+
 })();
