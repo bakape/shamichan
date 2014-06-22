4bf14134673914340ae75d751bab93f1478b6118
Lal'C Mellk Mal <lalc@doushio.com>
2014-06-21 17:58:04 -0700

    Lock to bottom with fuzziness

--- client/scroll.js
+++ client/scroll.js
@@ -75,7 +75,7 @@ oneeSama.hook('menuOptions', function (info) {
 });
 
 var at_bottom = function() {
-	return window.scrollY + window.innerHeight >= $DOC.height();
+	return window.scrollY + window.innerHeight >= $DOC.height() - 5;
 }
 if (window.scrollMaxY !== undefined)
 	at_bottom = function () {
