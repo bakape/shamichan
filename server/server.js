125631bfa091e57b3740f308cc4aac2dd0f24d70
Lal'C Mellk Mal <lalc@doushio.com>
2014-06-21 17:46:46 -0700

    write_thread_head takes options

--- server/server.js
+++ server/server.js
@@ -557,7 +557,10 @@ function (req, resp) {
 	var board = this.board, op = this.op;
 
 	resp = write_gzip_head(req, resp, this.headers);
-	render.write_thread_head(resp, board, op, this.subject, this.abbrev);
+	render.write_thread_head(resp, board, op, {
+		subject: this.subject,
+		abbrev: this.abbrev,
+	});
 
 	var opts = {fullPosts: true, board: board, loadAllPostsLink: true};
 	render.write_thread_html(this.reader, req, resp, opts);
