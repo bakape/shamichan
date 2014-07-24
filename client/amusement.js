984d6935bde0612eee3ee594f402a48f4fe155f3
bakape <scorpid33@gmail.com>
2014-07-24 23:31:09 +0300

    Fix r/a/dio banner. Remove redundant init script line.

--- client/amusement.js
+++ client/amusement.js
@@ -36,7 +36,7 @@ oneeSama.hook('insertOwnPost', function (extra) {
 	}
 });
 
-var bannerExtra = $.parseHTML('<b id="radioBanner"></b><script src="../js/radio-api-parse-v1.js"></script><script>rb_init();</script>', document, true);
+var bannerExtra = $.parseHTML('<b id="radioBanner"></b><script src="../static/js/radio-api-parse-v1.js"></script><script>rb_init();</script>', document, true);
 
 if (!$banner && bannerExtra) {
 	var dest;
