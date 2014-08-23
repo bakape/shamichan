15420ebb512a2717a298042687ac96eea2d150b1
bakape <scorpid33@gmail.com>
2014-08-23 16:37:51 +0400

    Split banner into left and center

--- www/js/radio-banner-v1.js
+++ www/js/radio-banner-v1.js
@@ -1,10 +1,24 @@
-function write_banner(){
+function write_banner(cb){
     $.getJSON('http://r-a-d.io/api', function(data){
         var main = data.main;
-        var info ='<b>' + '<a href="http://r-a-d.io/" target="_blank">' + '[' + main.listeners + '] ' +
-        	main.dj.djname + '</a>' + '&nbsp;&nbsp;' + main.np + '</b>';
-        document.getElementById('banner').innerHTML = info;
+        var info ='<a href="http://r-a-d.io/" target="_blank">' + '[' + main.listeners + '] ' +
+            main.dj.djname + '</a>' + '&nbsp;&nbsp;' + main.np;
+        document.getElementById('banner_center').innerHTML = info;
+	if (cb)
+            cb();
     });
 }
-write_banner();
+
+function banner_left(){
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "http://meguca.org/static/js/banner_left.html", true);
+    xhr.onload = function() {
+        var text = xhr.responseText;
+        document.getElementById('banner_left').innerHTML = '&nbsp;&nbsp;' + text;
+    }
+    xhr.send(null);
+}
+
+write_banner(banner_left);
 setInterval(write_banner, 10000);
+setInterval(banner_left, 60000);
