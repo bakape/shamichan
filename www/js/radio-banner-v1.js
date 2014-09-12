6f0103a1d2fb1cab734c429dd596324067e6cde7
Janis Petersons <bakape@gmail.com>
2014-09-12 23:42:15 +0200

    Use clicking for FAQ

--- www/js/radio-banner-v1.js
+++ www/js/radio-banner-v1.js
@@ -1,35 +1,39 @@
-var info;
-function write_banner(){
-    $.getJSON('https://r-a-d.io/api', function(data){
-        var main = data.main;
-        var new_info ='<a href="http://r-a-d.io/" target="_blank">' + '[' + main.listeners + '] ' +
-            main.dj.djname + '</a>' + '&nbsp;&nbsp;' + main.np;
-		if (new_info != info){
-			info = new_info;
-        	document.getElementById('banner_center').innerHTML = info;
-       	}
-    });
-}
-
-function build_faq(){
-	var answers = [
-		"Lewd is good", 
-		"No 3D porn", 
-		"Image size limit is 20 MB", 
-		"mumble.meguca.org:64738", 
-		"github.com/bakape/doushio", 
-		"The admin is drunk"
-	];
-	var list = ['<ul>'];
-	answers.forEach(function(entry){
-		list.push('<li>' + entry + '<li>');
-	});
-	list.push('<ul>');
-	document.getElementById('FAQ').innerHTML = list.join('');
-}
-
 window.onload = function(){
-	build_faq();
-	write_banner();
-	setInterval(write_banner, 10000);	
+	var info;
+	(function write_banner(){
+	    $.getJSON('https://r-a-d.io/api', function(data){
+	        var main = data.main;
+	        var new_info ='<a href="http://r-a-d.io/" target="_blank">' + '[' + main.listeners + '] ' +
+	            main.dj.djname + '</a>' + '&nbsp;&nbsp;' + main.np;
+			if (new_info != info){
+				info = new_info;
+	        	document.getElementById('banner_center').innerHTML = info;
+	       	}
+	       	setTimeout(write_banner, 10000);
+	    });
+	})();
+	
+	(function build_faq(){
+		var answers = [
+			"Lewd is good", 
+			"No 3D porn", 
+			"Image size limit is 20 MB", 
+			"mumble.meguca.org:64738", 
+			"github.com/bakape/doushio", 
+			"The admin is drunk"
+		];
+		var list = ['<ul>'];
+		answers.forEach(function(entry){
+			list.push('<li>' + entry + '<li>');
+		});
+		list.push('<ul>');
+		document.getElementById('FAQ').innerHTML = list.join('');
+	})();
 };
+
+function FAQ_onclick(){
+	if ($('#FAQ').css('display') == 'none')
+		$('#FAQ').show();
+	else
+		$('#FAQ').hide();
+}
