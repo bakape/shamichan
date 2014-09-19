15f04f5a483d97ecc6322939e7f836ccf0af621e
bakape <scorpid33@gmail.com>
2014-09-20 00:03:34 +0200

    Remove forkbomb

--- autoJoe/server.js
+++ autoJoe/server.js
@@ -1,24 +1,24 @@
 var request = require('request');
 
 (function autoJoe(){
-	var again = (function(){
+	var again = function(){
 		module.exports.isJoe = false;
 		module.exports.isSpecial = false;
 		module.exports.isJapanese = false;
 		module.exports.isMan = false;
 		setTimeout(autoJoe, 10000);
-	})();
+	};
 	var opts = {
 		url: 'https://r-a-d.io/api',
 		json: true,
 	};
 	request.get(opts, function (err, resp, json){
 		if (err)
-			return again;
+			return again();
 		if (resp.statusCode != 200)
-			return again;
+			return again();
 		if (!json || !json.main)
-			return again;
+			return again();
 		var song = json.main.np;
 		module.exports.isJoe = /Girls,? Be Ambitious/ig.test(song);
 		module.exports.isSpecial = /Super Special/ig.test(song);
