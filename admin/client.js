73a00e889934ec4eaf7d670ea91397f3c5aa834f
bakape <scorpid33@gmail.com>
2014-10-14 17:52:48 +0200

    Don't hide mnemonics in the admin panel

--- admin/client.js
+++ admin/client.js
@@ -482,7 +482,7 @@ function toggle_panel() {
 }
 
 // Togglle mnemonic display
-$('body').append('<style id="noMnemonics">.mod.addr{display:none;}</style>');
+$('body').append('<style id="noMnemonics">b>.mod.addr{display:none;}</style>');
 $('#noMnemonics').prop('disabled', !options.get('noMnemonics'));
 options.on('change:noMnemonics', function(model, noMnemonics){
 	$('#noMnemonics').prop('disabled', !noMnemonics);
