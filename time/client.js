81720dbc9a53934a27d23b084e952471d6f7691e
reiclone <reiclone@gmail.com>
2014-11-11 19:39:29 +0100

    syncwatch syncronised episode timer

--- time/client.js
+++ time/client.js
@@ -53,11 +53,47 @@ if (rTime){
 		setTimeout(function(){
 			$('time').each(function(){
 				var time = date_from_time_el(this).getTime();
-				$(this).text(relative_time(time, new Date().getTime()));
+				$(this).innerHTML(relative_time(time, new Date().getTime()));
 			});
 			increment_time();
 		} ,60000);
 	})();
 }
 
+})();
+
+function timer_from_el(el) {
+	var now = Date.now()
+	var start= el.getAttribute('start');
+	var diff=now-start;
+	var hour = Math.floor(diff/1000/60/60);
+	diff-= hour*1000*60*60;
+	var min= Math.floor(diff/1000/60);
+	diff-= min*1000*60;
+	var sec=Math.floor(diff/1000);
+	var maxh = el.getAttribute('hour');
+	var maxm = el.getAttribute('min');
+	var maxs = el.getAttribute('sec');
+	if((hour>maxh) || (hour==maxh && min>maxm) || (hour==maxh && min==maxm && sec>maxs)) //If we passed the time
+		return "Finished";
+	return "Now at: "+hour+":"+min+":"+sec+"\\"+maxh+":"+maxm+":"+maxs;
+}
+
+var syncwatch = (function(){
+	var el = document.querySelector('syncwatch');
+	return (el!=null);
+})();
+
+
+(function mouikkai(){
+	if (syncwatch){
+		setTimeout(function(){
+				$('syncwatch').each(function(){	
+					$(this).text(timer_from_el(this));
+				});
+				mouikkai();
+			} ,1000);
+	}else{
+		setTimeout(function(){mouikkai()} ,10000); //if there are no syncwatches we search for one every 10 secons
+	}
 })();
