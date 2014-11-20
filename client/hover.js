4dea53aad4c02fe8ea0ba9bfec5231231a512ed5
root <reiclone@gmail.com>
2014-11-20 04:48:16 +0100

    Use css instead of changing to article.

--- client/hover.js
+++ client/hover.js
@@ -35,7 +35,7 @@ function preview_miru(event, num) {
 
 			if (post.is('section'))
 				bits = bits.slice(0, 3);
-			preview = $('<article class="preview"/>').append(
+			preview = $('<div class="preview"/>').append(
 					bits.clone());
 			if((/^editing/).test(post[0].className))	//check if the post is being edited
 				preview[0].classList.add("editing");
