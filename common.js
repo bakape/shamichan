f387fc17999c852c2daa64e8175aaa6de8c9a28b
bakape <scorpid33@gmail.com>
2014-11-21 16:49:00 +0100

    Hot-reloadable illyaDance

--- common.js
+++ common.js
@@ -45,8 +45,6 @@ DEFINES.S_SPOIL = 3;
 // Expand configuration variables
 if (typeof mediaURL == 'undefined' || !mediaURL)
 	mediaURL = imagerConfig.MEDIA_URL;
-if (typeof illyaDance == 'undefined' || !illyaDance)
-	illyaDance = config.ILLYA_DANCE;
 if (typeof postedFrom == 'undefined' || !postedFrom)
 	postedFrom = config.POSTED_FROM;
 
