f6c1198cc824c559cec9fe4579d2bf3dfbf7f13d
bakape <scorpid33@gmail.com>
2015-01-17 11:36:59 +0100

    No fun by default

--- client/fun.js
+++ client/fun.js
@@ -1,12 +1,3 @@
 (function () {
-	function audio(vid, options) {
-		var $body = $('body');
-		if ($body.data('vid') == vid)
-			return;
-		$body.data({vid: vid}).find('.audio').remove();
-		if (!options)
-			options = {autoplay: '1', loop: '1'};
-		make_video(vid, options).css({'margin-left': '-9001px', 'position': 'absolute'}).addClass('audio').prependTo($body);
-	}
-	audio('BnC-cpUCdns');
+	
 })();
