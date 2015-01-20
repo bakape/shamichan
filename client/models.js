707c2908220ddcef841a1a0f2a2dfc04ce51ece6
reiclone <reiclone@gmail.com>
2015-01-20 06:03:55 +0100

    Fix renderRelativeTime

--- client/models.js
+++ client/models.js
@@ -131,9 +131,11 @@ var Article = Backbone.View.extend({
 			'change:image': this.renderImage,
 			'spoiler': this.renderSpoiler,
 			'removeSelf': this.bumplessRemove,
-			'add': this.renderRelativeTime,
-			'add': this.fun,
-		});
+			'add': function(){
+					this.renderRelativeTime();
+					this.fun();
+				},
+			});
 		this.commonListeners();
 		if (!options.get('postUnloading') && CurThread)
 			this.listenTo(this.model, {
