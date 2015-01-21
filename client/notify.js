8c20ce795b158f2f7346162e44e9998a5b276f84
bakape <scorpid33@gmail.com>
2015-01-21 21:54:22 +0100

    Remember seen replies

--- client/notify.js
+++ client/notify.js
@@ -3,14 +3,15 @@
 // Should be part of a greater thread model
 var Unread = new Backbone.Model({unreadCount: 0});
 
+// Remember replies that don't need a new desktop notification for 30 days
+var Replies = new Kioku('replies', 30);
+var readReplies = Replies.read_all();
+Replies.purge_expired_soon();
+
 var normalTitle = document.title;
 
 window.addEventListener('focus', function () {
-	Unread.set({blurred: false, unreadCount: 0, reply: false});
-}, false);
-
-window.addEventListener('blur', function () {
-	Unread.set({blurred: true, unreadCount: 0, reply: false});
+	Unread.set({unreadCount: 0, reply: false});
 }, false);
 
 connSM.on('synced', function () {
@@ -24,23 +25,29 @@ connSM.on('dropped', dropped);
 connSM.on('desynced', dropped);
 
 Backbone.on('repliedToMe', function (post) {
+	var num = post.get('num');
+	// Already read reply
+	if (readReplies[num])
+		return;
 	if (options.get('notification')) {
 		var body = post.get('body');
 		var image = post.get('image');
 		if((body || image) && document.hidden){
 			var n = new Notification('You have been quoted',{
-				// if the post doesn't have a image we us a bigger favicon
+				// if the post doesn't have a image we use a bigger favicon
 				icon: encodeURI(mediaURL+ (image ? 'thumb/'+image.thumb : '/css/ui/favbig.png')),
 				body: body,
 			});
 			n.onclick = function(){
 				window.focus();
-				location.hash = '#'+post.get('num');
+				location.hash = '#'+num;
 			};
 		}
 	}
 
 	Unread.set({reply: true});
+	// Record as already read
+	Replies.write(num, Replies.now());
 });
 Backbone.on('syncCountdown', function(time){
 	if (options.get('notification')) {
@@ -53,13 +60,13 @@ Backbone.on('syncCountdown', function(time){
 Backbone.on('afterInsert', function (model) {
 	if (model && model.get('mine'))
 		return; // It's ours, don't notify unread
-	if (Unread.get('blurred'))
+	if (document.hidden)
 		Unread.set('unreadCount', Unread.get('unreadCount') + 1);
 });
 
 Unread.on('change', function (model) {
 	var attrs = model.attributes;
-	if (!attrs.blurred) {
+	if (document.hidden) {
 		document.title = normalTitle;
 		return;
 	}
