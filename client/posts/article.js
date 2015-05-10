2f3aa06ad1201c39dea767d7bc7d7cd851654a24
bakape <scorpid33@gmail.com>
2015-05-10 18:06:47 +0200

    Remove legacy article code

--- client/posts/article.js
+++ client/posts/article.js
@@ -26,29 +26,15 @@ var Article = module.exports = Backbone.View.extend({
 		if (this.$el.is(':empty'))
 			this.render();
 		this.listenTo(this.model, {
-			'change:backlinks': this.renderBacklinks,
 			'change:editing': this.renderEditing,
 			'change:image': this.renderImage,
-			//removeSelf: this.bumplessRemove,
 			remove: this.remove
 		});
 		this.initCommon();
-		/* TEMP: Disabled for now
-		if (options.get('postUnloading') && state.page.get('thread')) {
-			this.listenTo(this.model, {
-				'add': unloadTopPost
-			});
-		}
-		*/
 	},
 
 	render: function () {
-		/*
-		 * Pass this model's links to oneeSama for renderring. The reason we
-		 * don't use the links attribute directly in OneeSama is different
-		 * rendering pathways on the server and client.
-		 * XXX: Unify this shit.
-		 */
+		// Pass this model's links to oneeSama for renderring
 		main.oneeSama.links = this.model.get('links');
 		this.setElement(main.oneeSama.mono(this.model.attributes));
 		// Insert into section
@@ -67,47 +53,8 @@ var Article = module.exports = Backbone.View.extend({
 
 	renderHide: function (model, hide) {
 		this.$el.toggle(!hide);
-	},
-
-	// To not shift the scroll position on remove
-	// TODO: Rework, once scrolling code is done
-	bumplessRemove: function(){
-		const pos = $(window).scrollTop();
-		if (!at_bottom() && this.$el.offset().top < pos)
-			// Not sure why we need the extra 2 pixels, but we do
-			$(window).scrollTop(pos - this.$el.outerHeight() - 2);
-		Posts.remove(this.model);
-		this.remove();
 	}
 });
 
 // Extend with common mixins
 _.extend(Article.prototype, imager.Hidamari, postCommon);
-
-// Keeps threads non-laggy by keeping displayed post count within lastN
-function unloadTopPost(){
-	var m = location.search.match(/last=(\d+)/);
-	const threadNum = state.page.get('thread');
-	if (!m
-		|| $(hover.mouseover.get('target')).is('a, img, video')
-		|| threadNum > 0
-	) {
-		return;
-	}
-	var thread = state.getThread(threadNum);
-	if (thread.replies.length <= parseInt(m[1], 10) + 5)
-		return;
-	state.posts.get(thread.replies.shift()).remove();
-	var $omit = $('.omit');
-	if (!$omit.length){
-		$omit = $('\t<span/>', {'class': 'omit'}).text(main.lang.abbrev_msg(1));
-		$omit.append(common.action_link_html(threadNum
-			+ location.hash, 'See all')+'\n');
-		$('section>blockquote').after($omit);
-	}
-	else {
-		var m = $omit.html().match(/^(\d+)(.*)/);
-		$('.omit').html(parseInt(m[1])+1+m[2]);
-	}
-	unloadTopPost();
-}
