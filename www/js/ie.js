1fa3235a1f1761bad42220fe8803d0eb4a2032b5
bakape <scorpid33@gmail.com>
2015-05-15 02:52:24 +0200

    Add browser compatibility check with warning message
    Closes bakape#184

--- www/js/ie.js
+++
@@ -1,5 +0,0 @@
-var h5s = ['abbr', 'aside', 'article', 'cite', 'code', 'figcaption', 'figure',
-		'nav', 'section', 'time'];
-for (var i = 0; i < h5s.length; i++)
-	document.createElement(h5s[i]);
-window.onload = function () { $('h1').after('<h2>Google Chrome recommended.</h2>'); };
--- www/js/setup.js
+++ www/js/setup.js
@@ -1,8 +1,26 @@
-// Set the theme. This needs to be done as fast as possible
-(function () {
-	'use strict';
+'use strict';
 
-	let options;
+// Check for browser compatibility by trying to detect some ES6 features
+(function() {
+	if (localStorage.browserChecked)
+		return;
+	if (typeof Set !== 'function'
+		|| 	typeof WeakSet !== 'function'
+		|| typeof Number.isNaN !== 'function'
+	) {
+		alert('Your browser appears to be outdated. In case you experience '
+			+ 'problems with browsing meguca, please consider installing the '
+			+ 'latest version of either Google Chrome, Mozilla Firefox, Opera,'
+			+ ' Microsoft Edge or any of the mobile versions of these browsers.'
+			+ 'This message will not appear again.'
+		);
+	}
+	localStorage.setItem('browserChecked', true);
+})();
+
+// Set the theme, if setting exists
+(function () {
+	var options;
 	try {
 		options = JSON.parse(localStorage.options);
 	}
@@ -10,8 +28,8 @@
 	if (!options)
 		options = {};
 
-	let link = document.getElementById('theme');
-	const m = link.href.match(/^(.*\/)[^\/]+?\.css$/),
+	var link = document.getElementById('theme'),
+		m = link.href.match(/^(.*\/)[^\/]+?\.css$/),
 		theme = options.theme;
 	if (m && theme)
 		link.href = m[1] + hotConfig.css[theme + '.css'];
