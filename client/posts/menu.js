effb75f3daa98fd4442d6b339e7ef03111d12d2d
bakape <scorpid33@gmail.com>
2015-05-28 06:19:22 +0200

    Close post menu on external click

--- client/posts/menu.js
+++ client/posts/menu.js
@@ -2,7 +2,8 @@
  Post action header menu
  */
 
-let _ = require('underscore'),
+let $ = require('jquery'),
+	_ = require('underscore'),
 	Backbone = require('backbone'),
 	main = require('../main'),
 	common = main.common,
@@ -32,6 +33,14 @@ let MenuView = module.exports = Backbone.View.extend({
 		html += '</ul>';
 		this.setElement(html);
 		this.$el.appendTo(parent);
+
+		// Remove view, if clicked outside
+		setTimeout(() => {
+			main.$doc.one('click', e => {
+				if (!$(e.target).closest('ul').is(this.$el))
+					this.remove();
+			})
+		}, 300);
 	},
 
 	// Forward post model to appropriate handler
