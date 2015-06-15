c99e3a54f636de551d5eca2af8286451345f9934
bakape <scorpid33@gmail.com>
2015-06-15 05:05:38 +0200

    Always check browser compatibility on load

--- www/js/loader.js
+++ www/js/loader.js
@@ -3,26 +3,23 @@ Selects and loads the client files
  */
 
 (function() {
-	if (!localStorage.legacyChecked) {
-		// Check for browser compatibility by trying to detect some ES6 features
-		if (typeof Set !== 'function'
-			|| 	typeof WeakSet !== 'function'
-			|| typeof Number.isNaN !== 'function'
-			|| !checkConst()
-			|| !checkTemplateStrings()
-		) {
-			// Load client with full ES5 complience
-			localStorage.legacy = true;
-		}
-		localStorage.legacyChecked = true;
-	}
+	// Check for browser compatibility by trying to detect some ES6 features
+	var legacy;
+	if (typeof Set !== 'function'
+		|| 	typeof WeakSet !== 'function'
+		|| typeof Number.isNaN !== 'function'
+		|| !checkConst()
+		|| !checkTemplateStrings()
+	)
+		// Load client with full ES5 complience
+		legacy = true;
 
 	var $script = require('scriptjs'),
 		base = config.MEDIA_URL + 'js/',
 		end = '.js?v=' + clientHash;
 
 	$script(base + 'lang/' + lang + end, function() {
-		var client = localStorage.legacy ? 'legacy' : 'client';
+		var client = legacy ? 'legacy' : 'client';
 		$script(base + client + end);
 	});
 
