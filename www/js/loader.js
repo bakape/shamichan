2348b835cf8d3a83e41f9819bfd58683042198d7
bakape <scorpid33@gmail.com>
2015-06-29 12:40:09 +0200

    Refactor ES6 support checks

--- www/js/loader.js
+++ www/js/loader.js
@@ -4,20 +4,37 @@ Selects and loads the client files
 
 (function () {
 	// Check for browser compatibility by trying to detect some ES6 features
+	function check(func) {
+		try {
+			return eval('(function(){' + func + '})();');
+		}
+		catch(e) {
+			return false;
+		}
+	}
+
+	var tests = [
+		// Constants
+		'"use strict"; const foo = 123; return foo === 123;',
+		// Block scoping
+		'"use strict";  const bar = 123; {const bar = 456;} return bar===123;',
+		// Template strings
+		'var a = "ba"; return `foo bar${a + "z"}` === "foo barbaz";',
+		// for...of
+		'var arr = [5]; for (var item of arr) return item === 5;'
+	];
 	var legacy;
-	if (typeof Set !== 'function'
-		|| typeof WeakSet !== 'function'
-		|| typeof Number.isNaN !== 'function'
-		|| !checkConst()
-		|| !checkTemplateStrings()
-	)
-		// Load client with full ES5 compliance
-		legacy = true;
+	for (var i = 0; i < tests.length; i++) {
+		if (!check(tests[i])) {
+			// Load client with full ES5 compliance
+			legacy = true;
+			break;
+		}
+	}
 
 	var $script = require('scriptjs'),
 		base = config.MEDIA_URL + 'js/',
 		end = '.js?v=' + clientHash;
-
 	$script(base + 'lang/' + lang + end, function() {
 		var client = legacy ? 'legacy' : 'client';
 		$script(base + client + end, function () {
@@ -28,26 +45,4 @@ Selects and loads the client files
 			}
 		});
 	});
-
-	function checkConst() {
-		try {
-			return eval('(function(){"use strict";const foo=123;return'
-				+ ' foo===123;})();'
-			);
-		}
-		catch(e) {
-			return false;
-		}
-	}
-
-	function checkTemplateStrings() {
-		try {
-			return eval('(function(){var a = "ba";'
-				+'return `foo bar${a + "z"}` === "foo barbaz";})();'
-			);
-		}
-		catch(e) {
-			return false;
-		}
-	}
 })();
