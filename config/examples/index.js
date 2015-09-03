defb810ac745601b61ba76a6a6acd2597d2e182c
bakape <scorpid33@gmail.com>
2015-09-03 03:05:51 +0300

    Remove legacy config and fix spelling

--- config/examples/index.js
+++ config/examples/index.js
@@ -87,8 +87,6 @@ module.exports = {
 // URL and domain of the website
 	PERSONA_AUDIENCE: 'http://localhost:8000',
 	LOGIN_SESSION_TIME: 60*60*24*14,
-// Enable staff to assign custom tags to poster IPs
-	IP_TAGGING: true,
 
 // r/a/dio integration (https://r-a-d.io)
 	RADIO: false,
