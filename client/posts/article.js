b11a1280b081c5be07975e7b3ce767be4c158d12
bakape <scorpid33@gmail.com>
2015-09-26 15:32:33 +0300

    Port article.js to new post DOM

--- client/posts/article.js
+++ client/posts/article.js
@@ -2,21 +2,20 @@
  * Reply posts
  */
 
-let main = require('../main'),
+const main = require('../main'),
 	postCommon = require('./common'),
-	{_, Backbone, options, state} = main;
+	{_, Backbone} = main;
 
-var Article = module.exports = Backbone.View.extend({
+const Article = module.exports = Backbone.View.extend({
 	tagName: 'article',
 	render() {
 		this.setElement(main.oneeSama.article(this.model.attributes));
 		return this;
 	},
 	insertIntoDOM() {
-		main.$threads.children('#p' + this.model.get('op'))
-			.children('blockquote, .omit, form, article[id]:last')
-			.last()
-			.after(this.$el);
+		const last = _.last(document.query('#p' + this.model.get('op'))
+			.queryAll('article[id]'));
+		last.after(this.el);
 		this.autoExpandImage().fun();
 	}
 });
