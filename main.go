9b9878fd424b0548624f040c4a61d81f99e85c91
bakape <scorpid33@gmail.com>
2015-12-15 20:30:11 +0200

    Refuse to run as root user

--- main.go
+++ main.go
@@ -1,6 +1,15 @@
 package main
 
+import (
+	"os/user"
+)
+
 func main() {
+	current, err := user.Current()
+	throw(err)
+	if current.Uid == "0" {
+		panic("Refusing to  run as root")
+	}
 	loadConfig()
 	loadLanguagePacks()
 	compileTemplates()
