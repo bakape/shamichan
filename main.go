d19036739004e8830c37fc3a1c5fca1390b2fce2
bakape <scorpid33@gmail.com>
2015-12-25 18:39:36 +0200

    Write PID files and kill already running instances

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"io/ioutil"
+	"log"
+	"os"
 	"os/user"
+	"strconv"
 )
 
 func main() {
@@ -11,8 +15,37 @@ func main() {
 		panic("Refusing to  run as root")
 	}
 	loadConfig()
+	pidFile()
 	loadLanguagePacks()
 	compileTemplates()
 	loadDB()
 	startServer()
+}
+
+// Handle any previous meguca processes and write down the cuurent PID to a file
+func pidFile() {
+	const path = "./.pid"
+
+	// Read old PID file
+	if buf, err := ioutil.ReadFile(path); err == nil {
+		// Kill previous meguca instance, if any
+		if !config.Hard.Debug {
+			pid, err := strconv.Atoi(string(buf))
+			throw(err)
+			process, err := os.FindProcess(pid)
+			throw(err)
+			if err := process.Kill(); err == nil {
+				log.Printf("Killed already running instance with PID %v\n", pid)
+			} else if err.Error() != "os: process already finished" {
+				panic(err)
+			}
+		}
+
+		throw(os.Remove(path))
+	} else if !os.IsNotExist(err) {
+		panic(err)
+	}
+
+	// Write new PID file
+	throw(ioutil.WriteFile(path, []byte(strconv.Itoa(os.Getpid())), 0660))
 }
