82b7ff349b4ff7bcb8b8e3f4b4b28620f46f373f
bakape <scorpid33@gmail.com>
2016-02-12 21:41:14 +0200

    legacy/src Remove already ported C++ addons

--- legacy/src/findapng.cpp
+++
@@ -1,9 +0,0 @@
-#include <nan.h>
-#include "apngDetector.h"
-
-using namespace v8;
-
-void Init(Handle<Object> exports) {
-	apngDetector::Init(exports);
-}
-NODE_MODULE(findapng,Init)
--- legacy/src/mnemonics.cpp
+++
@@ -1,9 +0,0 @@
-#include <nan.h>
-#include "mnemonizer.h"
-
-using namespace v8;
-
-void Init(Handle<Object> exports) {
-	mnemonizer::Init(exports);
-}
-NODE_MODULE(mnemonics,Init)
