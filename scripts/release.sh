e35e7c8f31f72a4fd3974b1cb9e10aa01d681afe
bakape <scorpid33@gmail.com>
2016-06-21 19:34:53 +0300

    scripts: Port release tagging script

--- scripts/release.sh
+++ scripts/release.sh
@@ -5,11 +5,9 @@
 
 version=$1
 
-npm update || exit 1
-npm install || exit 1
-npm version $version --no-git-tag-version || exit 1
 sed -i "s/##vNext/##${version} - $(date +%Y-%m-%d)/" CHANGELOG.md
-git commit CHANGELOG.md package.json -m $version || exit 1
+git commit CHANGELOG.md -m $version || exit 1
 git tag $version || exit 1
 git push || exit 1
 git push origin $version || exit 1
+make package
