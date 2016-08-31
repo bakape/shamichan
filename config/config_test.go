005025b1727fd0fe5676c019cb39f66600d30695
bakape <scorpid33@gmail.com>
2016-08-31 18:39:22 +0300

    config: Fix test regression

--- config/config_test.go
+++ config/config_test.go
@@ -56,6 +56,7 @@ func (*Tests) TestMarshalPublicBoardJSON(c *C) {
 	"hashCommands":false,
 	"notice":"",
 	"readOnly":true,
+	"rules":"",
 	"spoiler":"foo.png",
 	"textOnly":false,
 	"title":"Animu"
