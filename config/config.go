9bbfd89f648bf38256944c6b2ecd168559124449
bakape <scorpid33@gmail.com>
2016-08-31 19:20:23 +0300

    Revert "config: Increase default PNG conmpression to 30"
    This reverts commit 77cc718d71610963457a390873e08bbb07781ea5.

--- config/config.go
+++ config/config.go
@@ -42,7 +42,7 @@ var (
 		MaxThreads:    100,
 		MaxBump:       1000,
 		JPEGQuality:   80,
-		PNGQuality:    30,
+		PNGQuality:    20,
 		MaxSize:       5,
 		MaxHeight:     6000,
 		MaxWidth:      6000,
