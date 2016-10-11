71ae5ab0149b96f33bef4a2612dd658814ce5451
bakape <scorpid33@gmail.com>
2016-10-11 23:07:36 +0300

    templates: Refactor tests

--- templates/templates_test.go
+++ templates/templates_test.go
@@ -5,42 +5,43 @@ import (
 	"testing"
 
 	"github.com/bakape/meguca/config"
-	. "gopkg.in/check.v1"
+	. "github.com/bakape/meguca/test"
 )
 
-func Test(t *testing.T) { TestingT(t) }
-
-type Templates struct{}
-
-var _ = Suite(&Templates{})
-
-func (*Templates) SetUpTest(c *C) {
+func init() {
+	TemplateRoot = "testdata"
 	config.Set(config.Configs{
 		Boards: []string{"a"},
 	})
 }
 
-func (*Templates) TestBuildIndexTemplate(c *C) {
+func TestBuildIndexTemplate(t *testing.T) {
 	v := vars{
 		Config:     template.JS("c()"),
 		ConfigHash: "a",
 	}
-	source := `<script>{{.Config}}</script><b>{{.ConfigHash}}</b>` +
+	const source = `<script>{{.Config}}</script><b>{{.ConfigHash}}</b>` +
 		`{{.Navigation}}<script>{{.IsMobile}}</script>`
 	tmpl, err := template.New("index").Parse(source)
-	c.Assert(err, IsNil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
 	_, err = buildIndexTemplate(tmpl, v, false)
-	c.Assert(err, IsNil)
+	if err != nil {
+		t.Fatal(err)
+	}
 }
 
-func (*Templates) TestCompileTemplates(c *C) {
+func TestCompileTemplates(t *testing.T) {
 	config.SetClient([]byte{1}, "hash")
-	TemplateRoot = "testdata"
-	defer func() {
-		c.Assert(recover(), IsNil)
-	}()
-	resources = map[string]Store{}
-	c.Assert(Compile(), IsNil)
-	c.Assert(Get("index"), DeepEquals, resources["index"])
-	c.Assert(Get("mobile"), DeepEquals, resources["mobile"])
+	Set("index", Store{})
+	Set("mobile", Store{})
+
+	if err := Compile(); err != nil {
+		t.Fatal(err)
+	}
+	for _, k := range [...]string{"index", "mobile"} {
+		AssertDeepEquals(t, Get(k), resources[k])
+	}
 }
