84891b844855b05210509ca018284b2ef4a4efe6
bakape <scorpid33@gmail.com>
2016-10-12 19:27:06 +0300

    main: Refactor test

--- main_test.go
+++ main_test.go
@@ -1,24 +1,17 @@
-// Simple test, to see if the server starts
-
 package main
 
 import (
-	. "gopkg.in/check.v1"
 	"os"
 	"testing"
 )
 
-// Hook up gocheck into the "go test" runner.
-func Test(t *testing.T) { TestingT(t) }
-
-type Main struct{}
-
-var _ = Suite(&Main{})
-
-func (m *Main) TestServerStart(c *C) {
+// Simple test, to see if the server starts
+func TestServerStart(t *testing.T) {
 	os.Args = []string{os.Args[0], "init"}
 	defer func() {
-		c.Assert(recover(), IsNil)
+		if err := recover(); err != nil {
+			t.Fatal(err)
+		}
 	}()
 	main()
 }
