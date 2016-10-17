d688ca732fafe95b34bb2d912b981af59e56eac8
bakape <scorpid33@gmail.com>
2016-10-17 13:42:51 +0300

    imager/audio: Switch to refactored audio package

--- imager/audio.go
+++ imager/audio.go
@@ -5,7 +5,8 @@ import (
 	"io/ioutil"
 	"path/filepath"
 
-	"github.com/Soreil/audio"
+	"github.com/bakape/audio"
+	"github.com/bakape/video/avio"
 )
 
 // Directory for static image asset storage. Overridable for tests.
@@ -17,14 +18,15 @@ const fallbackCover = "audio-fallback.png"
 // Test if file is an MP3
 func detectMP3(buf []byte) (bool, error) {
 	d, err := audio.NewDecoder(bytes.NewReader(buf))
-	if err != nil {
-		if err == audio.DecoderError {
-			return false, nil
-		}
+	switch err {
+	case avio.ErrFailedAVFCtx, avio.ErrFailedAVIOCtx:
+		return false, nil
+	case nil:
+		defer d.Close()
+		return d.AudioFormat() == "mp3", nil
+	default:
 		return false, err
 	}
-	defer d.Destroy()
-	return d.AudioFormat() == "mp3", nil
 }
 
 // Extract image and meta info from MP3 files and send them down the
@@ -35,7 +37,7 @@ func processMP3(data []byte) (res thumbResponse) {
 		res.err = err
 		return
 	}
-	defer d.Destroy()
+	defer d.Close()
 	res.length = uint32(d.Duration() / 1000000000)
 
 	// No cover art in file. Assign fallback cover and return.
