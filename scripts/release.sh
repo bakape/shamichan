fb0db19b1b30db87cc2df0c970bbbdc88afbc0a4
bakape <scorpid33@gmail.com>
2016-10-23 15:06:54 +0300

    scripts/release: Pass static dep location to make

--- scripts/release.sh
+++ scripts/release.sh
@@ -10,4 +10,4 @@ git commit CHANGELOG.md -m $version || exit 1
 git tag $version || exit 1
 git push || exit 1
 git push origin $version || exit 1
-make update package cross_package_win_amd64
+make update package cross_package_win_amd64 "STATIC_ROOT="$STATIC_ROOT
