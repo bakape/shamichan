47b0d60f2b01548ed3fcdb0c003d24d34ae3cb0f
bakape <scorpid33@gmail.com>
2016-11-02 00:07:03 +0200

    scripts/release.sh: Commit readme changes

--- scripts/release.sh
+++ scripts/release.sh
@@ -6,7 +6,7 @@
 version=$1
 
 sed -i "s/##vNext/##${version} - $(date +%Y-%m-%d)/" CHANGELOG.md
-git commit CHANGELOG.md -m $version || exit 1
+git commit CHANGELOG.md README.md -m $version || exit 1
 git tag $version || exit 1
 git push || exit 1
 git push origin $version || exit 1
