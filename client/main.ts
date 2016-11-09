9ecbc2c2789aef18028a346167718982c35afba2
bakape <scorpid33@gmail.com>
2016-11-09 18:19:36 +0200

    client/main: Remove legacy cookie cleanup

--- client/main.ts
+++ client/main.ts
@@ -15,17 +15,6 @@ import { loadModule } from "./util"
 import { checkBottom, scrollToAnchor } from "./scroll"
 import bindMenu from "./posts/menu"
 
-// Clear cookies, if versions mismatch
-const cookieVersion = 4
-if (localStorage.getItem("cookieVersion") !== cookieVersion.toString()) {
-	for (let cookie of document.cookie.split(";")) {
-		const eqPos = cookie.indexOf("="),
-			name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie
-		document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT"
-	}
-	localStorage.setItem("cookieVersion", cookieVersion.toString())
-}
-
 // Load all stateful modules in dependency order
 async function start() {
 	// Load asynchronously and concurently as fast as possible
