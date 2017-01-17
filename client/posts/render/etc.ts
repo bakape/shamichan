0e019a79bcfdbbb99a5883e9b65e9bd733c4a724
bakape <scorpid33@gmail.com>
2017-01-17 08:30:19 +0200

    legacy: Remove  language packs

--- client/posts/render/etc.ts
+++ client/posts/render/etc.ts
@@ -29,23 +29,6 @@ export function renderPostLink(num: number, board: string, op: number): string {
     return html
 }
 
-// TODO: Reimplement, when moderation done
-
-// Render USER WAS BANNED FOR THIS POST message, or similar
-// export function renderBanned(): string {
-// 	return `<b class="admin banMessage">${lang.mod.banMessage}</b>`
-// }
-//
-// Render moderation information. Only exposed to authenticated staff.
-// export function renderModInfo(info) {
-// 	let html = '<b class="modLog admin">'
-// 	for (let action of info) {
-// 		html += `${lang.mod.formatLog(action)}<br>`
-// 	}
-// 	html += '</b>'
-// 	return html
-// }
-
 // Render links to posts that are linking to the target post
 export function renderBacklinks(post: DocumentFragment, links: PostLinks) {
     if (!links) {
