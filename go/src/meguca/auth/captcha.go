e1bcfa498c6f4d930e2f53e67e3e4cceb5624224
bakape <scorpid33@gmail.com>
2017-03-20 23:27:23 +0200

    auth: Split package more descriptively

---
+++ go/src/meguca/auth/captcha.go
@@ -0,0 +1,51 @@
+package auth
+
+import (
+	"encoding/json"
+	"log"
+	"meguca/config"
+	"net/http"
+	"net/url"
+)
+
+type captchaValidationResponse struct {
+	Success bool
+}
+
+// AuthenticateCaptcha posts a request to the SolveMedia API to authenticate a
+// captcha
+func AuthenticateCaptcha(captcha string) bool {
+	conf := config.Get()
+
+	// Captchas disabled or running tests. Can not use API, when testing
+	if !conf.Captcha {
+		return true
+	}
+	if captcha == "" {
+		return false
+	}
+
+	res, err := http.PostForm(
+		"https://www.google.com/recaptcha/api/siteverify",
+		url.Values{
+			"secret":   {conf.CaptchaPrivateKey},
+			"response": {captcha},
+		},
+	)
+	if err != nil {
+		printCaptchaError(err)
+		return false
+	}
+	defer res.Body.Close()
+
+	var data captchaValidationResponse
+	if err := json.NewDecoder(res.Body).Decode(&data); err != nil {
+		printCaptchaError(err)
+		return false
+	}
+	return data.Success
+}
+
+func printCaptchaError(err error) {
+	log.Printf("captcha: %s\n", err)
+}
