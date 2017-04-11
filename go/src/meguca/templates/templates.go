a28f3ac50d571e34802e9241e70ad7905d5fdf37
bakape <scorpid33@gmail.com>
2017-04-11 19:04:44 +0300

    templates: Fix omit count calculation

--- go/src/meguca/templates/templates.go
+++ go/src/meguca/templates/templates.go
@@ -80,7 +80,7 @@ func CalculateOmit(t common.Thread) (int, int) {
 	}
 
 	var (
-		omit    = int(t.PostCtr) - (len(t.Posts) + 1)
+		omit    = int(t.PostCtr) - len(t.Posts)
 		imgOmit uint32
 	)
 	if omit != 0 {
