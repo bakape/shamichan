a721fcd867fc44b5c2891b8b65e0d3af33312d9b
bakape <scorpid33@gmail.com>
2017-05-29 07:35:49 +0300

    client/posts/posting: Fix closing after buffered quote

--- client/posts/posting/index.ts
+++ client/posts/posting/index.ts
@@ -290,7 +290,7 @@ export default () => {
 	postSM.act(postState.draft, postEvent.done, (e?: Event) => {
 		// Commit a draft made as a non-live post
 		let commitNonLive = false
-		if (e) {
+		if (e && postModel.nonLive) {
 			if (e.target instanceof HTMLInputElement) {
 				commitNonLive = e.target.getAttribute("name") === "done"
 			} else if (e instanceof KeyboardEvent) {
