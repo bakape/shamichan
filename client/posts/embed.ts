27bd0b15229e0d68234c7ca74db9046ff3951dad
bakape <scorpid33@gmail.com>
2017-06-18 00:01:22 +0300

    cleint/posts/embed: Handle fetch errors

--- client/posts/embed.ts
+++ client/posts/embed.ts
@@ -1,8 +1,9 @@
 import { makeAttrs, makeFrag, escape, on, fetchJSON } from "../util"
 
 type OEmbedDoc = {
-	title: string
-	html: string
+	title?: string
+	html?: string
+	error?: string
 }
 
 // Types of different embeds by provider
@@ -60,10 +61,22 @@ function fetchNoEmbed(type: provider): (el: Element) => Promise<void> {
 		const url = "https://noembed.com/embed?url=" + el.getAttribute("href"),
 			[data, err] = await fetchJSON<OEmbedDoc>(url)
 		if (err) {
-			return console.warn(err)
+			el.textContent = format(err)
+			el.classList.add("errored")
+			console.error(err)
+			return
 		}
-		el.textContent = `[${provider[type]}] ${data.title}`
+		if (data.error) {
+			el.textContent = format(data.error)
+			el.classList.add("errored")
+			return
+		}
+		el.textContent = format(data.title)
 		el.setAttribute("data-html", encodeURIComponent(data.html.trim()))
+	}
+
+	function format(s: string): string {
+		return `[${provider[type]}] ${s}`
 	}
 }
 
@@ -96,12 +109,14 @@ function execFetcher(el: Element): Promise<void> {
 
 // Toggle the expansion of an embed
 async function toggleExpansion(e: MouseEvent) {
-	// Don't trigger, when user is trying to open in a new tab
-	if (e.which !== 1 || e.ctrlKey) {
+	const el = e.target as Element
+
+	// Don't trigger, when user is trying to open in a new tab or fetch has
+	// errored
+	if (e.which !== 1 || e.ctrlKey || el.classList.contains("errored")) {
 		return
 	}
 	e.preventDefault()
-	const el = e.target as Element
 
 	if (el.classList.contains("expanded")) {
 		el.classList.remove("expanded")
