111103ae751e126e21fd67964ab269d99ab95e31
bakape <scorpid33@gmail.com>
2017-07-03 21:27:24 +0300

    db: Fix disappearing images

--- go/src/meguca/db/sql/upkeep/delete_unused_images.sql
+++ go/src/meguca/db/sql/upkeep/delete_unused_images.sql
@@ -1,6 +1,6 @@
 delete from images
 	where (
-		select count(*) from posts
-			where SHA1 = images.SHA1
+		(select count(*) from posts where SHA1 = images.SHA1)
+		+ (select count(*) from image_tokens where SHA1 = images.SHA1)
 	) = 0
 	returning SHA1, fileType, thumbType
