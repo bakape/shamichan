994c8351afa397ffe12d9cabb5ecc9526681cd5f
bakape <scorpid33@gmail.com>
2017-07-07 23:02:57 +0300

    wasm: Stub for options

---
+++ src/options/mod.rs
@@ -0,0 +1,73 @@
+use std::default::Default;
+
+#[allow(non_snake_case)]
+pub struct Options {
+	hideThumbs: bool,
+	imageHover: bool,
+	webmHover: bool,
+	autogif: bool,
+	spoilers: bool,
+	notification: bool,
+	anonymise: bool,
+	postInlineExpand: bool,
+	relativeTime: bool,
+	nowPlaying: bool,
+	illyaDance: bool,
+	illyaDanceMute: bool,
+	horizontalPosting: bool,
+	hideRecursively: bool,
+	replyRight: bool,
+	workModeToggle: bool,
+	userBG: bool,
+	customCSSToggle: bool,
+	mascot: bool,
+	alwaysLock: bool,
+	newPost: u32,
+	toggleSpoiler: u32,
+	done: u32,
+	expandAll: u32,
+	workMode: u32,
+	inlineFit: String,
+	theme: String,
+	customCSS: String,
+}
+
+impl Default for Options {
+	fn default() -> Options {
+		Options {
+			hideThumbs: false,
+			imageHover: true,
+			webmHover: false,
+			autogif: false,
+			spoilers: true,
+			notification: true,
+			anonymise: false,
+			postInlineExpand: true,
+			relativeTime: false,
+			nowPlaying: false,
+			illyaDance: false,
+			illyaDanceMute: false,
+			horizontalPosting: false,
+			hideRecursively: false,
+			replyRight: false,
+			workModeToggle: false,
+			userBG: false,
+			customCSSToggle: false,
+			mascot: false,
+			alwaysLock: false,
+			newPost: 78,
+			toggleSpoiler: 73,
+			done: 83,
+			expandAll: 69,
+			workMode: 66,
+			inlineFit: String::from("width"),
+			theme: String::from("moe"), // TODO: Read from configs
+			customCSS: String::new(),
+		}
+	}
+}
+
+fn load_options() -> Options {
+	let mut opts = Options::default();
+	opts
+}
