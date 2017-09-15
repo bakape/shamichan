6b4bbfa35ae19fc789a8da3c08af1ea1ed47c3c9
bakape <scorpid33@gmail.com>
2017-09-15 22:32:58 +0300

    db: Persist moderator image spoilering through cache

--- go/src/meguca/db/sql/admin/mod_spoiler_image.sql
+++ go/src/meguca/db/sql/admin/mod_spoiler_image.sql
@@ -1,4 +1,5 @@
 update posts
 	set spoiler = true
 	where id = $1
-	returning log_moderation(4::smallint, board, $1, $2)
+	returning log_moderation(4::smallint, board, $1, $2),
+		bump_thread(op, false, false, false)
