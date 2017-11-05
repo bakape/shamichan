eb9e28796ecba1fe54bfa7409d702c77fe09a2b5
bakape <scorpid33@gmail.com>
2017-11-05 21:16:49 +0200

    wasm: Open post body rendering

--- client_cpp/src/posts/body.cc
+++ client_cpp/src/posts/body.cc
@@ -1,10 +1,17 @@
+#include "../lang.hh"
+#include "../state.hh"
 #include "models.hh"
+#include <optional>
 #include <string>
 #include <string_view>
-#include <vector>
+#include <tuple>
 
 using std::string_view;
 using std::string;
+using std::optional;
+using std::nullopt;
+using std::tuple;
+using std::get;
 
 Node Post::render_body()
 {
@@ -73,10 +80,162 @@ Node Post::render_body()
     return n;
 }
 
-// TODO
+// Return, if b is a punctuation char
+static bool is_punctuation(const char b)
+{
+    switch (b) {
+    case '!':
+    case '"':
+    case '\'':
+    case '(':
+    case ')':
+    case ',':
+    case '-':
+    case '.':
+    case ':':
+    case ';':
+    case '?':
+    case '[':
+    case ']':
+        return true;
+    default:
+        return false;
+    }
+}
+
+// Splits off one byte of leading and trailing punctuation, if any, and returns
+// the 3 split parts. If there is no edge punctuation, the respective char
+// is null.
+static tuple<char, string_view, char> split_punctuation(const string_view word)
+{
+    tuple<char, string_view, char> re = { 0, word, 0 };
+
+    // Split leading
+    if (word.size() < 2) {
+        return re;
+    }
+    if (is_punctuation(word[0])) {
+        get<0>(re) = word[0];
+        get<1>(re) = word.substr(1);
+    }
+
+    // Split trailing
+    const size_t l = get<1>(re).size();
+    if (l < 2) {
+        return re;
+    }
+    if (is_punctuation(get<1>(re).back())) {
+        get<2>(re) = get<1>(re).back();
+        get<1>(re) = get<1>(re).substr(0, l - 1);
+    }
+
+    return re;
+}
+
+// Parses link to a post.
+// If valid, returns number of extra '>' in front of the link and ID of the
+// post, the link is pointing to.
+static optional<tuple<int, uint64_t>> parse_post_link(string_view word)
+{
+    // Count leading '>'
+    int count = 0;
+    while (word.size()) {
+        if (word[0] == '>') {
+            count++;
+            word = word.substr(1);
+        } else {
+            break;
+        }
+    }
+    if (count < 2) {
+        return nullopt;
+    }
+    count -= 2;
+
+    // Verify everything else is digits
+    if (!word.size()) {
+        return nullopt;
+    }
+    int i = 0;
+    while (i < word.size()) {
+        if (word[i] < '0' || word[i] > '9') {
+            return nullopt;
+        }
+        i++;
+    }
+
+    return { { count, std::stoull(string(word)) } };
+}
+
+// Render a temporary link for open posts
+static Node render_temp_link(uint64_t id)
+{
+    const string id_str = std::to_string(id);
+    string text = ">>" + id_str;
+    if (post_ids->mine.count(id)) {
+        text += ' ';
+        text += lang->posts.at("you");
+    }
+    return {
+        "a",
+        {
+            { "class", "post-link temp" }, { "data-id", id_str },
+            { "href", "#p" + id_str },
+        },
+        text,
+    };
+}
+
+// Parse temporary links in open posts, that still may be edited
 void Post::parse_temp_links(string_view frag)
 {
-    state.append({ "span", string(frag), true });
+    bool first = true;
+    string buf;
+    buf.reserve(frag.size());
+
+    parse_string(frag, " ",
+        [this, &first, &buf](auto frag) {
+            if (!first) {
+                buf += ' ';
+            } else {
+                first = false;
+            }
+
+            // Split leading and trailing punctuation, if any
+            auto && [ lead_punct, word, trail_punct ] = split_punctuation(frag);
+            if (lead_punct) {
+                buf += lead_punct;
+            }
+
+            bool matched = false;
+            if (word.size() && word[0] == '>') {
+                if (auto l = parse_post_link(word); l) {
+                    // Text preceding the link
+                    auto[count, id] = *l;
+                    for (int i = 0; i < count; i++) {
+                        buf += '>';
+                    }
+                    state.append({ "span", buf, true });
+                    buf.clear();
+
+                    state.append(render_temp_link(id));
+                    matched = true;
+                }
+            }
+            if (!matched) {
+                buf += word;
+            }
+
+            if (trail_punct) {
+                buf += trail_punct;
+            }
+        },
+        []() {});
+
+    // Append any leftover text
+    if (buf.size()) {
+        state.append({ "span", buf, true });
+    }
 }
 
 // TODO
