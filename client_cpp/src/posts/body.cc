7031e4bc29311ddf5389f6ff309970d7afb18dcc
bakape <scorpid33@gmail.com>
2017-11-05 19:08:06 +0200

    wasm: Fix line parser

--- client_cpp/src/posts/body.cc
+++ client_cpp/src/posts/body.cc
@@ -6,25 +6,6 @@
 using std::string_view;
 using std::string;
 
-// Splits string into string_views
-static std::vector<string_view> split_string(
-    const string& str, const string delimiter)
-{
-    std::vector<string_view> views;
-
-    size_t pos = 0;
-    size_t prev = 0;
-    while ((pos = str.find(delimiter, prev)) != -1) {
-        views.push_back({ &str[prev], pos - prev });
-        prev = pos + 1;
-    }
-
-    // To get the last substring (or only, if delimiter is not found)
-    views.push_back({ &str[prev], str.size() - pos });
-
-    return views;
-}
-
 Node Post::render_body()
 {
     Node n("blockquote");
@@ -34,60 +15,61 @@ Node Post::render_body()
     state.reset(&n);
 
     bool first = true;
-    for (auto&& line : split_string(body, "\n")) {
-        state.quote = false;
+    parse_string(string_view(body), "\n",
+        [this, &first](string_view line) {
+            state.quote = false;
 
-        // Prevent successive empty lines
-        if (!first) {
-            if (state.successive_newlines < 2) {
-                state.append({ "br" });
+            // Prevent successive empty lines
+            if (!first) {
+                if (state.successive_newlines < 2) {
+                    state.append({ "br" });
+                }
+            } else {
+                first = false;
             }
-        } else {
-            first = false;
-        }
-        if (!line.size()) {
-            state.successive_newlines++;
-            continue;
-        }
+            if (!line.size()) {
+                state.successive_newlines++;
+                return;
+            }
 
-        state.successive_newlines = 0;
-        if (line[0] == '>') {
-            state.quote = true;
-            state.append({ "em" }, true);
-        }
-        if (state.spoiler) {
-            state.append({ "del" }, true);
-        }
-        if (state.bold) {
-            state.append({ "b" }, true);
-        }
-        if (state.italic) {
-            state.append({ "i" }, true);
-        }
+            state.successive_newlines = 0;
+            if (line[0] == '>') {
+                state.quote = true;
+                state.append({ "em" }, true);
+            }
+            if (state.spoiler) {
+                state.append({ "del" }, true);
+            }
+            if (state.bold) {
+                state.append({ "b" }, true);
+            }
+            if (state.italic) {
+                state.append({ "i" }, true);
+            }
 
-        if (editing) {
-            parse_code(
-                line, [this](string_view frag) { parse_temp_links(frag); });
-        } else {
-            parse_code(
-                line, [this](string_view frag) { parse_fragment(frag); });
-        }
+            if (editing) {
+                parse_code(
+                    line, [this](string_view frag) { parse_temp_links(frag); });
+            } else {
+                parse_code(
+                    line, [this](string_view frag) { parse_fragment(frag); });
+            }
 
-        // Close any unclosed tags
-        if (state.italic) {
-            state.ascend();
-        }
-        if (state.bold) {
-            state.ascend();
-        }
-        if (state.spoiler) {
-            state.ascend();
-        }
-        if (state.quote) {
-            state.ascend();
-        }
-    }
-
+            // Close any unclosed tags
+            if (state.italic) {
+                state.ascend();
+            }
+            if (state.bold) {
+                state.ascend();
+            }
+            if (state.spoiler) {
+                state.ascend();
+            }
+            if (state.quote) {
+                state.ascend();
+            }
+        },
+        []() {});
     return n;
 }
 
