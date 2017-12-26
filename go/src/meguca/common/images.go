d696faf4dde1404bcb8c486a3976e81342a319b2
bakape <scorpid33@gmail.com>
2017-12-26 13:54:54 +0200

    websockets: Fix image insertion in open posts

--- go/src/meguca/common/images.go
+++ go/src/meguca/common/images.go
@@ -1,4 +1,4 @@
-//go:generate easyjson --all $GOFILE
+//go:generate easyjson --all -no_std_marshalers $GOFILE
 
 package common
 
--- go/src/meguca/common/posts.go
+++ go/src/meguca/common/posts.go
@@ -1,4 +1,4 @@
-//go:generate easyjson --all $GOFILE
+//go:generate easyjson --all -no_std_marshalers $GOFILE
 
 // Package common contains common shared types, variables and constants used
 // throughout the project
