cf0b2fb55c879ae7339c9a8a78222eb095dc48bf
bakape <scorpid33@gmail.com>
2018-01-02 06:30:47 +0200

    imager: Time out thumbnailing tasks, that take too long

--- go/src/meguca/imager/scheduler.go
+++ go/src/meguca/imager/scheduler.go
@@ -5,9 +5,12 @@ import (
 	"crypto/sha1"
 	"database/sql"
 	"encoding/hex"
+	"errors"
+	"meguca/common"
 	"meguca/db"
 	"mime/multipart"
 	"runtime"
+	"time"
 
 	"github.com/bakape/thumbnailer"
 )
@@ -36,37 +39,53 @@ func init() {
 	for i := 0; i < n; i++ {
 		go func() {
 			for {
+				// Perform thumbnailing in separate goroutine, so we can time
+				// out the request after 10 seconds. Unsure why this happens,
+				// but lengthy requests should not block workers.
 				req := <-requestThumbnailing
-
-				buf := bytes.NewBuffer(thumbnailer.GetBuffer())
-				_, err := buf.ReadFrom(req.file)
-				data := buf.Bytes()
-				if err != nil {
-					req.res <- thumbnailingResponse{500, "", err}
-					thumbnailer.ReturnBuffer(data)
-					continue
+				to := time.After(time.Second * 10)
+				ch := make(chan thumbnailingResponse, 1)
+				go processRequest(req.file, ch)
+				select {
+				case res := <-ch:
+					req.res <- res
+				case <-to:
+					req.res <- thumbnailingResponse{
+						500,
+						"",
+						errors.New("thumbnailing timed out"),
+					}
 				}
-
-				sum := sha1.Sum(data)
-				SHA1 := hex.EncodeToString(sum[:])
-				img, err := db.GetImage(SHA1)
-				var (
-					code int
-					id   string
-				)
-				switch err {
-				case nil: // Already have a thumbnail
-					code, id, err = newImageToken(SHA1)
-				case sql.ErrNoRows:
-					img.SHA1 = SHA1
-					code, id, err = newThumbnail(data, img)
-				default:
-					code = 500
-				}
-
-				req.res <- thumbnailingResponse{code, id, err}
-				thumbnailer.ReturnBuffer(data)
 			}
 		}()
 	}
+}
+
+func processRequest(file multipart.File, ch chan<- thumbnailingResponse) {
+	var res thumbnailingResponse
+
+	buf := bytes.NewBuffer(thumbnailer.GetBuffer())
+	_, res.err = buf.ReadFrom(file)
+	data := buf.Bytes()
+	defer thumbnailer.ReturnBuffer(data)
+	if res.err != nil {
+		res.code = 500
+		ch <- res
+		return
+	}
+
+	sum := sha1.Sum(data)
+	SHA1 := hex.EncodeToString(sum[:])
+	var img common.ImageCommon
+	img, res.err = db.GetImage(SHA1)
+	switch res.err {
+	case nil: // Already have a thumbnail
+		res.code, res.imageID, res.err = newImageToken(SHA1)
+	case sql.ErrNoRows:
+		img.SHA1 = SHA1
+		res.code, res.imageID, res.err = newThumbnail(data, img)
+	default:
+		res.code = 500
+	}
+	ch <- res
 }
