ddaf09b7356f10722bf1a81a270dcf7c8372cbf1
bakape <scorpid33@gmail.com>
2018-04-26 00:46:28 +0300

    parser: Remove pyu tests

--- go/src/meguca/parser/commands_test.go
+++ go/src/meguca/parser/commands_test.go
@@ -3,7 +3,6 @@ package parser
 import (
 	"meguca/common"
 	"meguca/config"
-	"meguca/db"
 	. "meguca/test"
 	"testing"
 )
@@ -69,49 +68,4 @@ func Test8ball(t *testing.T) {
 	if val != answers[0] && val != answers[1] {
 		t.Fatalf("unexpected answer: %s", val)
 	}
-}
-
-func TestPyu(t *testing.T) {
-	if err := db.SetPyu(0); err != nil {
-		t.Fatal(err)
-	}
-
-	t.Run("disabled", func(t *testing.T) {
-		(*config.Get()).Pyu = false
-		for _, in := range [...]string{"pyu", "pcount"} {
-			com, err := parseCommand([]byte(in), "a")
-			if err != nil {
-				t.Error(err)
-			}
-			AssertDeepEquals(t, com, common.Command{})
-		}
-	})
-
-	t.Run("enabled", func(t *testing.T) {
-		(*config.Get()).Pyu = true
-
-		cases := [...]struct {
-			name, in string
-			Type     common.CommandType
-			Val      uint64
-		}{
-			{"count on zero", "pcount", common.Pcount, 0},
-			{"increment", "pyu", common.Pyu, 1},
-			{"count", "pcount", common.Pcount, 1},
-		}
-
-		for i := range cases {
-			c := cases[i]
-			t.Run(c.name, func(t *testing.T) {
-				com, err := parseCommand([]byte(c.in), "a")
-				if err != nil {
-					t.Fatal(err)
-				}
-				AssertDeepEquals(t, com, common.Command{
-					Type: c.Type,
-					Pyu:  c.Val,
-				})
-			})
-		}
-	})
 }
