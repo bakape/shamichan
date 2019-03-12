a474e58bae1475f57e34aa1d4ac057ee66be0760
bakape <bakape@gmail.com>
2019-03-12 21:32:39 +0200

    client: Don't keep fetching image, after success

--- client/posts/posting/image.ts
+++ client/posts/posting/image.ts
@@ -1,29 +1,29 @@
 // Reload image if error
 // Handle event if image error
-async function onImageErr(e: Event) {
+function onImageErr(e: Event) {
     const el = (e.target as HTMLImageElement)
 
     if (el.tagName !== "IMG"
         || (el.complete && el.naturalWidth !== 0)
-        || el.getAttribute("data-handling-error")
-    ) {
+        || el.getAttribute("data-scheduled-retry")) {
         return
     }
-    el.setAttribute("data-handling-error", "1");
-
 
     e.stopPropagation()
     e.preventDefault()
 
-    for (var i = 0; i < 30; i++) {
-        if (el.complete && el.naturalWidth !== 0) {
-            break
-        }
+    el.setAttribute("data-scheduled-retry", "1");
+    setTimeout(() => retry(el), 2000);
+}
 
-        // Force refresh the cache
-        el.src = el.src;
-        await new Promise(resolve => setTimeout(resolve, 2000))
+// Retry download
+function retry(el: HTMLImageElement) {
+    if (!document.contains(el) || el.naturalWidth !== 0) {
+        el.removeAttribute("data-scheduled-retry");
+        return;
     }
+    el.src = el.src;
+    setTimeout(() => retry(el), 2000);
 }
 
 // Bind listeners
