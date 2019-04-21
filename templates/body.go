d8fdb83de7fcb9f3a52d5c3e92b71003538751c8
bakape <bakape@gmail.com>
2019-04-21 14:18:31 +0300

    templates: Fix formatting

--- templates/body.go
+++ templates/body.go
@@ -381,8 +381,8 @@ func (c *bodyContext) parseFragment(frag string) {
 		if leadPunct != 0 {
 			c.byte(leadPunct)
 		}
-		if (strings.Count(word, "(") == strings.Count(word, ")")+1) && 
-		(trailPunct == ')') && (strings.Contains(word, "http")) {
+		if (strings.Count(word, "(") == strings.Count(word, ")")+1) &&
+			(trailPunct == ')') && (strings.Contains(word, "http")) {
 			word += ")"
 			trailPunct = ' '
 		}
