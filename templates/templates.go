4dd432570c7f8f3a5931c1260a54e39bb269a3f5
bakape <bakape@gmail.com>
2019-06-09 20:09:43 +0300

    temlates: Remove post rendering

--- templates/templates.go
+++ templates/templates.go
@@ -5,13 +5,9 @@ package templates
 
 import (
 	"bytes"
-	"fmt"
-	"html"
-	"io"
 	"sync"
 
 	"github.com/bakape/meguca/common"
-	"github.com/bakape/meguca/config"
 	"github.com/bakape/meguca/util"
 )
 
@@ -53,49 +49,4 @@ func Recompile() error {
 	}
 
 	return nil
-}
-
-// Board writes board HTML to w
-func Board(w io.Writer, b, theme string, page, total int,
-	pos common.ModerationLevel, minimal, catalog bool, threadHTML []byte,
-) {
-	conf := config.GetBoardConfigs(b)
-	title := html.EscapeString(fmt.Sprintf("/%s/ - %s", b, conf.Title))
-	write := func(w io.Writer) {
-		writerenderBoard(w, threadHTML, b, title, conf, page, total, pos,
-			catalog)
-	}
-
-	if minimal {
-		write(w)
-	} else {
-		execIndex(w, title, theme, pos, write)
-	}
-}
-
-// Thread writes thread page HTML
-func Thread(w io.Writer, id uint64, board, title, theme string, abbrev,
-	locked bool, pos common.ModerationLevel, postHTML []byte,
-) {
-	title = html.EscapeString(fmt.Sprintf("/%s/ - %s", board, title))
-	execIndex(w, title, theme, pos, func(w io.Writer) {
-		writerenderThread(w, postHTML, id, board, abbrev, locked, pos)
-	})
-}
-
-// Execute and index template in the second pass
-func execIndex(w io.Writer, title, theme string, pos common.ModerationLevel,
-	fn func(w io.Writer),
-) {
-	mu.RLock()
-	t := indexTemplates[pos]
-	mu.RUnlock()
-
-	w.Write(t[0])
-	w.Write([]byte(title))
-	w.Write(t[1])
-	w.Write([]byte(theme))
-	w.Write(t[2])
-	fn(w)
-	w.Write(t[3])
 }
--- templates/util.go
+++ templates/util.go
@@ -2,29 +2,7 @@ package templates
 
 import (
 	"html"
-
-	"github.com/bakape/meguca/common"
 )
-
-// CalculateOmit returns the omitted post and image counts for a thread
-func CalculateOmit(t common.Thread) (int, int) {
-	var (
-		omit    = int(t.PostCount) - (len(t.Posts) + 1)
-		imgOmit uint32
-	)
-	if omit != 0 {
-		imgOmit = t.ImageCount
-		if t.Image != nil {
-			imgOmit--
-		}
-		for _, p := range t.Posts {
-			if p.Image != nil {
-				imgOmit--
-			}
-		}
-	}
-	return omit, int(imgOmit)
-}
 
 func bold(s string) string {
 	s = html.EscapeString(s)
