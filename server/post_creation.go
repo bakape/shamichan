b9bb9933cae0045c569739c95be070b59ad73653
bakape <bakape@gmail.com>
2019-07-01 00:28:38 +0300

    server: Don't make addMine cookie domain-specific

--- server/post_creation.go
+++ server/post_creation.go
@@ -43,10 +43,9 @@ func createThread(w http.ResponseWriter, r *http.Request) {
 
 		// Let the JS add the ID of the post to "mine"
 		http.SetCookie(w, &http.Cookie{
-			Name:   "addMine",
-			Value:  strconv.FormatUint(post.ID, 10),
-			Path:   "/",
-			Domain: config.RootDomain(),
+			Name:  "addMine",
+			Value: strconv.FormatUint(post.ID, 10),
+			Path:  "/",
 		})
 
 		http.Redirect(w, r, fmt.Sprintf(`/%s/%d`, req.Board, post.ID), 303)
