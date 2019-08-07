2173bd0d7366ae0c6b1c5cf3ef158d743661e488
Edison Lee <edisonlee@edisonlee55.com>
2019-08-07 22:40:19 +0800

    lang: Add zh_TW

--- common/vars.go
+++ common/vars.go
@@ -50,7 +50,7 @@ const (
 var (
 	Langs = []string{
 		"en_GB", "es_ES", "fr_FR", "nl_NL", "pl_PL", "pt_BR", "sk_SK", "tr_TR",
-		"uk_UA", "ru_RU",
+		"uk_UA", "ru_RU", "zh_TW",
 	}
 	Themes = []string{
 		"ashita", "console", "egophobe", "gar", "glass", "gowno", "higan",
