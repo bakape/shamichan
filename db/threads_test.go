1256b5f4c939de742105e58b6693ed73299d3878
bakape <bakape@gmail.com>
2019-12-13 11:32:35 +0200

    db: Add test

--- db/threads_test.go
+++ db/threads_test.go
@@ -1,6 +1,10 @@
 package db
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/bakape/meguca/test"
+)
 
 func TestInsertThread(t *testing.T) {
 	id, err := InsertThread("test", []string{"anime", "mango"}, genToken(t))
@@ -10,4 +14,16 @@ func TestInsertThread(t *testing.T) {
 	if id == 0 {
 		t.Fatal("id not set")
 	}
+
+	exists, err := ThreadExists(id)
+	if err != nil {
+		t.Fatal(err)
+	}
+	test.AssertEquals(t, exists, true)
+
+	exists, err = ThreadExists(456636351)
+	if err != nil {
+		t.Fatal(err)
+	}
+	test.AssertEquals(t, exists, false)
 }
