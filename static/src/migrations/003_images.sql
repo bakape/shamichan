0ec2be7c375a6b6b68cb5e65bb19fdd4eaadd8f9
bakape <bakape@gmail.com>
2019-12-27 12:31:40 +0200

    sql: Unpack image dims array

--- static/src/migrations/003_images.sql
+++ static/src/migrations/003_images.sql
@@ -22,16 +22,25 @@ create type file_type as enum (
 	'CBR'
 );
 
+create domain uint15 as smallint check (value >= 0);
+
 create table images (
 	sha1 bytea primary key check (octet_length(sha1) = 20),
-	md5 bytea not null check(octet_length(md5) = 16),
+	md5 bytea not null check (octet_length(md5) = 16),
 
 	audio bool not null,
 	video bool not null,
+
 	file_type file_type not null,
 	thumb_type file_type not null,
-	dims smallint[] not null check(array_length(dims, 1) = 4),
+
+	width uint15 not null,
+	height uint15 not null,
+	thumb_width uint15 not null,
+	thumb_height uint15 not null,
+
 	size int not null check (size > 0),
+
 	title varchar(200) not null,
 	artist varchar(100) not null
 );
