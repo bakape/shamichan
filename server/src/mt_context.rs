352140a25877e1b229f9b040b5ea1b2f38b50a40
bakape <bakape@gmail.com>
2021-02-01 00:08:52 +0200

    server: fix tokio runtime panic

--- server/src/mt_context.rs
+++ server/src/mt_context.rs
@@ -17,7 +17,7 @@ lazy_static! {
 	/// Global multithreaded Tokio runtime
 	static ref TOKIO_RUNTIME: tokio::runtime::Runtime =
 		tokio::runtime::Builder::new_multi_thread()
-		.enable_time()
+		.enable_all()
 		.build()
 		.unwrap();
 }
