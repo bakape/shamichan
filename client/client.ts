1485c404229c29df3f5d152bf21da570ae5da4b1
nukooo <58382191+nukooo@users.noreply.github.com>
2021-02-07 21:13:01 -0500

    client: disallow non-http(s) redirection

--- client/client.ts
+++ client/client.ts
@@ -140,8 +140,12 @@ export default () => {
 		handle(msg.id, m =>
 			m.applyModeration(msg))
 
-	handlers[message.redirect] = (url: string) =>
-		location.href = url
+	handlers[message.redirect] = (msg: string) => {
+		const url = new URL(msg, location.origin)
+		if (/https?:/.test(url.protocol)) {
+			location.href = url.href
+		}
+	}
 
 	handlers[message.notification] = (text: string) =>
 		new OverlayNotification(text)
