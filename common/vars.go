228fb73b0708bf37ecdf1e98c8769cea434bffc1
bakape <bakape@gmail.com>
2021-07-04 14:59:24 +0300

    assets: increase max banner count to 100 per board

--- common/vars.go
+++ common/vars.go
@@ -37,7 +37,7 @@ const (
 	MaxLenRules        = 5000
 	MaxLenEightball    = 2000
 	MaxLenReason       = 100
-	MaxNumBanners      = 20
+	MaxNumBanners      = 100
 	MaxAssetSize       = 100 << 10
 	MaxDiceSides       = 10000
 	BumpLimit          = 1000
@@ -86,8 +86,10 @@ var (
 
 // Common Regex expressions
 var (
-	CommandRegexp = regexp.MustCompile(`^#(flip|\d*d\d+|8ball|pyu|pcount|sw(?:\d+:)?\d+:\d+(?:[+-]\d+)?|autobahn)$`)
-	DiceRegexp    = regexp.MustCompile(`(\d*)d(\d+)`)
+	CommandRegexp = regexp.MustCompile(
+		`^#(flip|\d*d\d+|8ball|pyu|pcount|sw(?:\d+:)?\d+:\d+(?:[+-]\d+)?|autobahn)$`,
+	)
+	DiceRegexp = regexp.MustCompile(`(\d*)d(\d+)`)
 )
 
 func init() {
